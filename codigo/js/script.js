446d68a81c9e24f42c45ce42fe89c9a22bbf5f21
Angel-06 <L17112011@cdjuarez.tecnm.mx>
2020-10-20 14:51:29 -0600

    js

---
+++ codigo/js/script.js
@@ -0,0 +1,24 @@
+console.log(5);
+console.log("Hola muando!" .toUpperCase());
+console.log("Hola muando!" .length);
+
+var prim = "Hola", seg = "Mundo";
+console.log("%s, %s!", prim, seg);
+console.log("El carácter en el índice 0 es '" + prim.charAt(0) + "'")
+
+
+console.log(234.456);
+console.log(2+3);
+console.log(34-12);
+//Tipos de datos: Numericos, String, Boolean, null, underfined, symbol, Object
+//Variables:
+var x =10;
+x = 'Hola';
+let y = 15;
+const vacia = false;
+
+var myArr = ["Mexico", "Alemania", "China", "Francia" ];
+console.log(myArr);
+/*
+comentarios
+*/
